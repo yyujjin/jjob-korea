a2afda4503bf989384c339a3fa8b607279c4995d
yyujjin <aba1740@naver.com>
2024-07-01 22:27:07 +0900

    MySql 연동

--- src/test/java/com/jjobkorea/JJobKoreaApplicationTests.java
+++ src/test/java/com/jjobkorea/JJobKoreaApplicationTests.java
@@ -1,13 +1,34 @@
 package com.jjobkorea;
 
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class JJobKoreaApplicationTests {
 
+	@Autowired
+	private ApplicationContext context;
+	
+	@Autowired
+	private SqlSessionFactory sessionFactory;
+	
 	@Test
 	void contextLoads() {
 	}
-
+	
+	@Test
+	public void testByApplicationContext() {
+		try {
+			System.out.println("=========================");
+			System.out.println(context.getBean("sqlSessionFactory"));
+			System.out.println("=========================");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
