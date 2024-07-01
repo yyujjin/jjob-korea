21319ff03171c04e4fdcade3b785175f973d02ef
yyujjin <aba1740@naver.com>
2024-07-01 21:38:27 +0900

    프로젝트 생성

---
+++ src/main/java/com/jjobkorea/JJobKoreaApplication.java
@@ -0,0 +1,13 @@
+package com.jjobkorea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JJobKoreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JJobKoreaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jjobkorea/JJobKoreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjobkorea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JJobKoreaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
