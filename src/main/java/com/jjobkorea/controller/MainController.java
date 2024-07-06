dd7a4f729f21d594e6573918d2bd81a62023b2be
yyujjin <aba1740@naver.com>
2024-07-06 13:44:18 +0900

    페이지 첫 로드시 채용 리스트 불러오기 문제 수정, 불필요 코드 삭제

--- src/main/java/com/jjobkorea/controller/MainController.java
+++ src/main/java/com/jjobkorea/controller/MainController.java
@@ -19,72 +19,37 @@ public class MainController {
 
 	@Autowired
 	private JobPostingService jobPostingService;
-	
-	
-	// 메인 페이지로 이동
+
+	// 메인 페이지
 	@GetMapping("/main")
-	public String enterMain() {
+	public String enterMain(Model model) {
 		log.info("메인 페이지 진입");
+
+		List<JobPostingDTO> postingList = jobPostingService.getPostingList();
+		model.addAttribute("postingList", postingList);
 		return "main/main";
 	}
-	
-	
-	// 메인 컨텐츠 페이지 
-		@GetMapping("/main/main-content")
-		public String enterMainContent() {
-			log.info("메인 컨텐트 진입");
-			return "/main/main-content";
-		}
-	
 
+	
 	// 요청 페이지 받기
 	@GetMapping("requestPage/{page}")
 	public String requestPage(@PathVariable("page") String page, Model model) {
 
 		log.info("요청 페이지 -> " + page);
-		
+
+		//메인 컨텐츠
 		if (page.equals("main")) {
-			
+
 			List<JobPostingDTO> postingList = jobPostingService.getPostingList();
 			model.addAttribute("postingList", postingList);
-			
+
 			page = "main/main-content";
 		}
 
-
+		
 		model.addAttribute("page", page);
 		// 요청 페이지 내보내기
 		return "main/main";
-	}
-
-	@GetMapping("/login")
-	public String enterlogin() {
-		log.info("로그인 페이지 진입");
-		return "login";
-	}
-
-	@GetMapping("/signUp")
-	public String enterSignUp() {
-		log.info("회원가입 페이지 진입");
-		return "signUp";
-	}
-
-	@GetMapping("/jobPosting")
-	public String enterjobPosting() {
-		log.info("채용 정보 페이지 진입");
-		return "jobPosting";
-	}
-
-	@GetMapping("/jobSeekerBoard")
-	public String enterJobSeekerBoard() {
-		log.info("구직자 게시판 진입");
-		return "jobSeekerBoard";
-	}
-
-	@GetMapping("/companyBoard")
-	public String entercompanyBoard() {
-		log.info("기업 게시판 진입");
-		return "companyBoard";
 	}
 
 }
