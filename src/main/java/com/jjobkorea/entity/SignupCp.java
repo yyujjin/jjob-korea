17ef262f951fa377b9716ff14bdb5fee862ff070
ddukung02 <yooiwn@gmail.com>
2024-07-09 19:10:22 +0900

    설정 변경

--- src/main/java/com/jjobkorea/entity/SignupCp.java
+++ src/main/java/com/jjobkorea/entity/SignupCp.java
@@ -1,12 +1,12 @@
 package com.jjobkorea.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
-
 
 @Entity
 @Table(name = "signup_cp")
@@ -16,10 +16,21 @@ public class SignupCp {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "companyType")
     private String companyType;
+
+    @Column(name = "companyRegistrationNum")
     private String companyRegistrationNum;
+
+    @Column(name = "companyName")
     private String companyName;
+
+    @Column(name = "companyRepresentativeName")
     private String companyRepresentativeName;
+
+    @Column(name = "companyAddr")
     private String companyAddr;
+
+    @Column(name = "companyBusinessRegistration")
     private String companyBusinessRegistration;
 }
