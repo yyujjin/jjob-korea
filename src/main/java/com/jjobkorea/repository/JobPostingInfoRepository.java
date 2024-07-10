656539c2a06973d4808459ab01d0bc093e605231
ddukung02 <yooiwn@gmail.com>
2024-07-10 14:31:00 +0900

    필요없는 파일 삭제

--- src/main/java/com/jjobkorea/repository/JobPostingInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jjobkorea.repository;
-
-import com.jjobkorea.entity.JobPostingInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JobPostingInfoRepository extends JpaRepository<JobPostingInfo, Integer> {
-}
