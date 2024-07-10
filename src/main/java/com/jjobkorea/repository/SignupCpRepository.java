748ff921e713450466e50ee924d9955498f8ff10
ddukung02 <yooiwn@gmail.com>
2024-07-10 11:52:33 +0900

    레파짓토리 에러 코드 변경

--- src/main/java/com/jjobkorea/repository/SignupCpRepository.java
+++ src/main/java/com/jjobkorea/repository/SignupCpRepository.java
@@ -2,10 +2,8 @@ package com.jjobkorea.repository;
 
 import com.jjobkorea.entity.SignupCp;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-<<<<<<< HEAD
+@Repository
 public interface SignupCpRepository extends JpaRepository<SignupCp, Long> {
-=======
-public interface SignupCpRepository extends JpaRepository<SignupCp, Integer> {
->>>>>>> 48854a02a74857e1fd7e0388d9c10cca4883aec9
 }
