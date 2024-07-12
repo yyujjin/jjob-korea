0690c2f7cb7d40777876a2113bf3476f2f42d21f
yyujjin <aba1740@naver.com>
2024-07-12 23:47:08 +0900

    불필요 주석 삭제

--- src/main/java/com/jjobkorea/service/JobPostingServiceImpl.java
+++ src/main/java/com/jjobkorea/service/JobPostingServiceImpl.java
@@ -46,44 +46,73 @@ public class JobPostingServiceImpl implements JobPostingService {
 		return getPostingList;
 	}
 
-	
-	//조건 검색
+	// 조건 검색
 	@Override
-	public List<JobPostingDTO> getSearchList(List<String> filterList, FilterDTO dto ) {
+	public List<JobPostingDTO> getSearchList(List<String> filterList, FilterDTO dto) {
 		log.info("getSearchList 서비스 실행됨!!");
-		
-		for(String filter : filterList) {
-			
+
+		for (String filter : filterList) {
+
 			switch (filter) {
-			case "프론트엔드" : dto.setFrontEnd(filter); break;
-			case "백엔드" : dto.setBackEnd(filter); break;
-			case "풀스택" : dto.setFullStack(filter); break;
-			case "서울" : dto.setSeoul(filter); break;
-			case "부산" : dto.setBusan(filter); break;
-			case "대구" : dto.setDaegu(filter); break;
-			case "인천" : dto.setDaegu(filter); break;
-			case "광주" : dto.setGwangju(filter); break;
-			case "대전" : dto.setDaejeon(filter); break;
-			case "울산" : dto.setUlsan(filter); break;
-			case "세종" : dto.setSejong(filter); break;
-			case "대기업" : dto.setLargeCompany(filter); break;
-			case "중소기업" : dto.setSmallMediumCompany(filter); break;
-			case "스타트업" : dto.setStartup(filter); break;
-			case "정규직" : dto.setRegularEmployee(filter); break;
-			case "계약직" : dto.setContractEmployee(filter); break;
-			case "인턴" : dto.setIntern(filter); break;
+			case "프론트엔드":
+				dto.setFrontEnd(filter);
+				break;
+			case "백엔드":
+				dto.setBackEnd(filter);
+				break;
+			case "풀스택":
+				dto.setFullStack(filter);
+				break;
+			case "서울":
+				dto.setSeoul(filter);
+				break;
+			case "부산":
+				dto.setBusan(filter);
+				break;
+			case "대구":
+				dto.setDaegu(filter);
+				break;
+			case "인천":
+				dto.setDaegu(filter);
+				break;
+			case "광주":
+				dto.setGwangju(filter);
+				break;
+			case "대전":
+				dto.setDaejeon(filter);
+				break;
+			case "울산":
+				dto.setUlsan(filter);
+				break;
+			case "세종":
+				dto.setSejong(filter);
+				break;
+			case "대기업":
+				dto.setLargeCompany(filter);
+				break;
+			case "중소기업":
+				dto.setSmallMediumCompany(filter);
+				break;
+			case "스타트업":
+				dto.setStartup(filter);
+				break;
+			case "정규직":
+				dto.setRegularEmployee(filter);
+				break;
+			case "계약직":
+				dto.setContractEmployee(filter);
+				break;
+			case "인턴":
+				dto.setIntern(filter);
+				break;
 			}
 		}
-		
-		
-		
-		//필터 dto qhsorl 
+
+		// 필터 dto qhsorl
 		List<FilterDTO> searchList = jobPostingMapper.getSearchList(dto);
-		log.info("받아온 searchList{} :",  searchList);
-		
+		log.info("받아온 searchList{} :", searchList);
+
 		return null;
 	}
-
-	
 
 }
