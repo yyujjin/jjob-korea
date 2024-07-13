961195b3871e9a84e203dbe9928f69edeef12932
yyujjin <aba1740@naver.com>
2024-07-13 16:34:00 +0900

    선택된 필터 조건에 맞는 리스트만 서비스단에 가져오기 완료

--- src/main/java/com/jjobkorea/controller/JobPostingController.java
+++ src/main/java/com/jjobkorea/controller/JobPostingController.java
@@ -6,16 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.jjobkorea.dto.FilterDTO;
 import com.jjobkorea.dto.JobPostingDTO;
 import com.jjobkorea.service.JobPostingService;
 
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
