adae9fddd2ebe964ab854925b8080c67a2b003c2
yyujjin <aba1740@naver.com>
2024-07-15 18:24:40 +0900

    :fire: 불필요 코드 삭제

--- src/main/java/com/jjobkorea/controller/JobPostingController.java
+++ src/main/java/com/jjobkorea/controller/JobPostingController.java
@@ -26,9 +26,6 @@ public class JobPostingController {
 	@GetMapping("jobPosting")
 	public String enterJobPosting(HttpServletRequest request, HttpServletResponse response, Model model) {
 		int pageNum=0;
-		//리퀘스트 객체에서 요청에 대한 데이터 정보들이 같이 넘어온다.
-		//여기서 보이지 않을 뿐 디버깅 걸면 보임
-		//그러므로 request 자체가 null일 수 없다.
 		if(request.getParameter("pageNum")==null) {
 			pageNum =1;
 		}else{
@@ -36,24 +33,13 @@ public class JobPostingController {
 		}
 
 		log.info("요청받은 페이지: {}", pageNum);
-		String page = "jobPosting/jobPostingMain";
-		model.addAttribute("page", page);
-
-		// 채용 정보 리스트 가져오는 메서드 실행시키기
-		getPostingList(model);
-
-		return "main/main";
-	}
-
-	// 채용 정보 페이지 진입 시 리스트 가져오기
-	public String getPostingList(Model model) {
-		log.info("채용정보 리스트 가져오기");
-
-		// 채용 정보 서비스단 연결
 		List<JobPostingDTO> postingList = jobPostingService.getPostingList();
 		model.addAttribute("postingList", postingList);
 
-		return "jobPosting/jobList";
+		String page = "jobPosting/jobPostingMain";
+		model.addAttribute("page", page);
+
+		return "main/main";
 	}
 
 }
