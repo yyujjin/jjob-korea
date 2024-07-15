7f839954a123c1cdf46acd52ac1191e699b62599
mkh4014 <167828774+mkh4014@users.noreply.github.com>
2024-07-15 16:45:47 +0900

    회원가입 시 개인아이디에서 기업 아이디 로그인 되는거 수정 및 푸터와 스타일 겹치는거 수정

--- src/main/java/com/jjobkorea/mapper/MemMapper.java
+++ src/main/java/com/jjobkorea/mapper/MemMapper.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 import com.jjobkorea.dto.MemDTO;
 
@@ -14,6 +13,4 @@ public interface MemMapper {
     public ArrayList<MemDTO> selectByCpId(HashMap<String, String> param);
     public void write(HashMap<String, String> param);
     public int checkCpRegistrationNum(String cpRegistrationNum);
-    
-    
 }
