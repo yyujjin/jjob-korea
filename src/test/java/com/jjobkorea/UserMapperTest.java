a461bd954dd79bdca1922e5c9ac33861aa0efbe8
yyujjin <aba1740@naver.com>
2024-07-26 14:38:39 +0900

    :white_check_mark:아이디 불일치 시 로그인 실패 테스트 코드 추가

--- src/test/java/com/jjobkorea/UserMapperTest.java
+++ src/test/java/com/jjobkorea/UserMapperTest.java
@@ -10,6 +10,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.util.AssertionErrors.assertNotNull;
 
 
@@ -30,5 +33,15 @@ public class UserMapperTest {
         String userName = userMapper.findUsernameByLoginInfo(userDTO);
         assertNotNull(userName,"유저 이름이 null이 아니어야 합니다.");
         log.info("로그인 성공, 유저네임 : {}",userName);
+    }
+
+    @Test
+    public void 로그인실패_아이디_불일치() {
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUserId("aaa");
+        userDTO.setPassword("1234");
+        String userName = userMapper.findUsernameByLoginInfo(userDTO);
+        assertNull(userName,"유저 이름이 null이어야 합니다.");
     }
 }
