45e2c083358b2cc2a611afafd5d2443314e4c247
yyujjin <aba1740@naver.com>
2024-07-26 14:08:42 +0900

    :art: 매퍼 어노테이션 적용

--- src/main/java/com/jjobkorea/mapper/UserMapper.java
+++ src/main/java/com/jjobkorea/mapper/UserMapper.java
@@ -1,7 +1,9 @@
 package com.jjobkorea.mapper;
 
 import com.jjobkorea.dto.UserDTO;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserMapper {
     //아이디, 비번이 일치하면 유저의 이름을 가져오는 기능
     public String findUsernameByLoginInfo(UserDTO userDTO);
