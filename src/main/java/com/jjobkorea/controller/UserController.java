dc89dad5438b9a2a4df6f4b5ec26f7d034ef9ae7
yyujjin <aba1740@naver.com>
2024-07-26 15:49:40 +0900

    :sparkles: 로그인 처리 기능 추가: UserController 구현

---
+++ src/main/java/com/jjobkorea/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.jjobkorea.controller;
+
+import com.jjobkorea.dto.UserDTO;
+import com.jjobkorea.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    public final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    //로그인
+    @GetMapping("/login")
+    public String login(UserDTO userDTO) {
+        String userName = userService.findUsernameByLoginInfo(userDTO);
+
+        if (userName != null){ //로그인 성공
+            return "main/main";
+        }else { //로그인 실패
+            return "redirect:user/login";
+        }
+    }
+}
