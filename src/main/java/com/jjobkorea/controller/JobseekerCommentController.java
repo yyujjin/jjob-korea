542fc3dcc428507ec0a46e78bb9d3e510aca1f2d
DarWook <m4768@naver.com>
2024-07-26 11:19:35 +0900

    기능 추가: 댓글 작성 시 로그인을 요구하고, 로그인하지 않으면 로그인 페이지로 이동

--- src/main/java/com/jjobkorea/controller/JobseekerCommentController.java
+++ src/main/java/com/jjobkorea/controller/JobseekerCommentController.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.jjobkorea.dto.JobseekerCommentDTO;
 import com.jjobkorea.service.JobseekerCommentService;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
@@ -25,10 +30,15 @@ public class JobseekerCommentController {
 	private JobseekerCommentService service;
 	
 	@PostMapping("/jobseekerSave")
-	public @ResponseBody ArrayList<JobseekerCommentDTO> save(@RequestParam HashMap<String, String> param) {
+	public @ResponseBody ArrayList<JobseekerCommentDTO> save(@RequestParam HashMap<String, String> param
+			// 로그인세션
+			,HttpSession session) { 
 		log.info("@# save");
 		log.info("@# param=>"+param);
-		
+		// 로그인 안했을때 예외처리
+		if (session.getAttribute("user") == null) {
+            throw new IllegalStateException("로그인 페이지로 이동합니다.");
+        }
 		service.save(param);
 		
 		// 해당 게시글에 작성된 댓글 리스트를 가져옴
@@ -43,6 +53,15 @@ public class JobseekerCommentController {
         param.put("jobseekerCommunityBoardNum", String.valueOf(jobseekerCommunityBoardNum));
         return service.findAll(param);
     }
+	
+	// 로그인을 안한 예외를 받아서 로그인 페이지로 이동
+	@ControllerAdvice
+	public static class GlobalExceptionHandler {
+	    @ExceptionHandler(IllegalStateException.class)
+	    public ResponseEntity<String> handleIllegalStateException(IllegalStateException ex) {
+	    	return new ResponseEntity<>("redirect:/login", HttpStatus.UNAUTHORIZED);
+	    }
+	}
 	
 }
 
