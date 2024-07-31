e6fd3e9a1b403bcf22021d58a7e089b6101c82a0
mkh4014 <167828774+mkh4014@users.noreply.github.com>
2024-07-31 16:26:47 +0900

    Update MainController.java
    불필요한 주석 삭제

--- src/main/java/com/jjobkorea/controller/MainController.java
+++ src/main/java/com/jjobkorea/controller/MainController.java
@@ -28,10 +28,6 @@ public class MainController {
     
     @Autowired
     private AddJobPostingController addJobPostingController;
-
-    // 로그인, 회원가입 컨트롤러
-//    @Autowired
-//    private MemController memController;
     
     //리팩토링 코드
     @Autowired
@@ -91,8 +87,7 @@ public class MainController {
 
             // 회원가입 페이지 진입
             case "register":
-//                return memController.register(model);
-//                return userController.register(model);
+               return userController.register(model);
             // 채용 정보 페이지 진입
             case "jobPosting":
                 return jobPostingController.enterJobPosting(request, model);
