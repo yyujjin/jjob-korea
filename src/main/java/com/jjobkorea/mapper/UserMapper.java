b557e89689f4b94f11f360d3c4618bde3b3a6627
mkh4014 <167828774+mkh4014@users.noreply.github.com>
2024-07-31 14:24:30 +0900

    회원가입 코드 refactory 1차 수정

--- src/main/java/com/jjobkorea/mapper/UserMapper.java
+++ src/main/java/com/jjobkorea/mapper/UserMapper.java
@@ -1,12 +1,23 @@
 package com.jjobkorea.mapper;
 
-import com.jjobkorea.dto.UserDTO;
+import java.util.HashMap;
+
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.Optional;
+import com.jjobkorea.dto.UserDTO;
 
 @Mapper
 public interface UserMapper {
     //아이디, 비번이 일치하면 유저의 이름을 가져오는 기능
     public UserDTO login(UserDTO userDTO);
+    
+    //아이디 존재 여부 확인
+    public int checkUserId(String userId);
+    
+    //구직자 회원 가입
+    public void individualUser(HashMap<String, String> param);
+    //기업 회원가입
+    public void companyUser(HashMap<String, String> param);
+
+    public void insertCompanyInfo(HashMap<String, String> param);
 }
