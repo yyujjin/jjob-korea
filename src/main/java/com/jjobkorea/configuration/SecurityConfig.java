348548c8400c684c4a861d394eac7619bc1aa5f8
yyujjin <aba1740@naver.com>
2024-08-03 19:04:11 +0900

    :sparkles: 로그인 없이 접근 가능한 경로("/public/**") 설정

--- src/main/java/com/jjobkorea/configuration/SecurityConfig.java
+++ src/main/java/com/jjobkorea/configuration/SecurityConfig.java
@@ -2,7 +2,22 @@ package com.jjobkorea.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 
+        httpSecurity
+                .authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/public/**").permitAll()
+        );
+        return httpSecurity.build();
     }
+
+}
