a2f76ee0860ee40ed93206e728aab5e4ff31cdc4
yyujjin <aba1740@naver.com>
2024-08-03 18:46:53 +0900

    :sparkles: 스프링 시큐리티 설정 클래스 생성

---
+++ src/main/java/com/jjobkorea/configuration/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.jjobkorea.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class SecurityConfig {
+
+    }
