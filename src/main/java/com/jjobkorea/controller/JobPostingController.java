5ea4341e077f44bb781372064b1813d7606054fe
yyujjin <aba1740@naver.com>
2024-08-04 20:33:52 +0900

    :recycle: 공고등록 엔드포인트 변경

--- src/main/java/com/jjobkorea/controller/JobPostingController.java
+++ src/main/java/com/jjobkorea/controller/JobPostingController.java
@@ -47,7 +47,7 @@ public class JobPostingController {
     }
 
     //공고 등록 페이지
-    @GetMapping("/jobPost/new")
+    @GetMapping("/jobPost/create")
     public String createJobPost(Model model) {
 
         model.addAttribute("page","jobPostingDetails/addJobPosting");
