a17e31a20ed76086b3490c4dedf55184388e8017
yyujjin <aba1740@naver.com>
2024-08-05 23:52:25 +0900

    :sparkles: Service 어노테이션 추가

--- src/main/java/com/jjobkorea/service/CustomUserDetailsService.java
+++ src/main/java/com/jjobkorea/service/CustomUserDetailsService.java
@@ -6,7 +6,9 @@ import com.jjobkorea.mapper.UserMapper;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserMapper userMapper;
