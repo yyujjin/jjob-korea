f957a6e550ed939f92155e7dce93abd11471ea23
yyujjin <aba1740@naver.com>
2024-08-05 17:29:03 +0900

    :sparkles:  특정 경로에 대한 접근 허용 및 CSRF 비활성화

--- src/main/java/com/jjobkorea/configuration/SecurityConfig.java
+++ src/main/java/com/jjobkorea/configuration/SecurityConfig.java
@@ -1,10 +1,34 @@
 package com.jjobkorea.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 
+        httpSecurity.authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/WEB-INF/views/**").permitAll()
+                .requestMatchers("/", "/login", "/register").permitAll() //메인, 로그인, 회원가입
+                .requestMatchers("/jobPosts", "/searchFilter").permitAll() //채용 정보
+                .requestMatchers(
+                        "/jobseekerContent_view",
+                                "/requestPage/jobseekerContent_view",
+                                "/getComments",
+                                "/board",
+                                "/display",
+                                "/getFileList",
+                                "/download").permitAll() //게시판,
+
+                .anyRequest().authenticated()
+        );
+        //csrf 사용 x
+        httpSecurity.csrf((auth) -> auth.disable());
+        return httpSecurity.build();
+    }
 }
