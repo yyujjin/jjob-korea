998c946d02052a7d776e40581b081a8e88b1f637
yyujjin <aba1740@naver.com>
2024-08-06 01:37:12 +0900

    :sparkles: Service 어노테이션 추가

--- src/main/java/com/jjobkorea/service/UserSessionService.java
+++ src/main/java/com/jjobkorea/service/UserSessionService.java
@@ -1,7 +1,9 @@
 package com.jjobkorea.service;
 
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserSessionService {
     public String getUserId() {
         return SecurityContextHolder.getContext().getAuthentication().getName();
