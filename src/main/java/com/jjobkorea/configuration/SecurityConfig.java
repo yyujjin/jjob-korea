981201a0f836087dd728804d1c9a965137114cb3
yyujjin <aba1740@naver.com>
2024-08-13 20:31:02 +0900

    :sparkles: OAuth config 경로 추가

--- src/main/java/com/jjobkorea/configuration/SecurityConfig.java
+++ src/main/java/com/jjobkorea/configuration/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.jjobkorea.configuration;
 
+import com.jjobkorea.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +13,12 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+        this.customOAuth2UserService = customOAuth2UserService;
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -59,8 +67,19 @@ public class SecurityConfig {
                 .sessionManagement((auth) -> auth
                 .sessionFixation().changeSessionId());
 
+        httpSecurity
+                .httpBasic((basic)->basic.disable());// HTTP 기본 인증 비활성화
+
+        httpSecurity
+                .oauth2Login((oauth2) -> oauth2
+                .loginPage("/login") //커스텀 로그인 페이지 띄우기
+                .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
+                .userService(customOAuth2UserService)));
+
         //csrf 사용 x
         httpSecurity.csrf((auth) -> auth.disable());
         return httpSecurity.build();
     }
 }
+
+// SecurityConfig에 등록
