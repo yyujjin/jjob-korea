b449e0397655d8c87f9d8b9707826c0a2af7d7a8
yyujjin <aba1740@naver.com>
2024-08-13 17:13:41 +0900

    :sparkles: CustomOAuth2User 작성

---
+++ src/main/java/com/jjobkorea/dto/CustomOAuth2User.java
@@ -0,0 +1,46 @@
+package com.jjobkorea.dto;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+public class CustomOAuth2User implements OAuth2User {
+    private final OAuth2Response oAuth2Response;
+    private final String role;
+
+    public CustomOAuth2User(OAuth2Response oAuth2Response, String role) {
+        this.oAuth2Response = oAuth2Response;
+        this.role = role;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return Map.of();
+    }
+
+    //role 값에 해당
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return role;
+            }
+        });
+        return collection;
+    }
+
+    //사용자의 별명이나 이름 값
+    @Override
+    public String getName() {
+        return oAuth2Response.getName();
+    }
+
+    public String getUserName() {
+        return oAuth2Response.getProvider()+" "+oAuth2Response.getProviderId();
+    }
+}
