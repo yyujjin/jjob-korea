4b2178b58115333bbeed9f4d51d8d86e99c29aa0
yyujjin <aba1740@naver.com>
2024-08-14 10:18:44 +0900

    :sparkles:  CustomOAuth2User 추가

--- src/main/java/com/jjobkorea/controller/MainController.java
+++ src/main/java/com/jjobkorea/controller/MainController.java
@@ -2,7 +2,9 @@ package com.jjobkorea.controller;
 
 import java.util.List;
 
+import com.jjobkorea.dto.CustomOAuth2User;
 import com.jjobkorea.service.UserSessionService;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,9 +25,13 @@ import lombok.extern.slf4j.Slf4j;
 public class MainController {
 
     private final JobPostingService jobPostingService;
+    private final CustomOAuth2User customOAuth2User;
+    private final UserSessionService userSessionService;
 
-    public MainController(JobPostingService jobPostingService, UserSessionService userSessionService) {
+    public MainController(JobPostingService jobPostingService, UserSessionService userSessionService, CustomOAuth2User customOAuth2User) {
         this.jobPostingService = jobPostingService;
+        this.customOAuth2User = customOAuth2User;
+        this.userSessionService = userSessionService;
     }
 
     // 메인
@@ -36,6 +42,9 @@ public class MainController {
         List<JobPostingDTO> postingList = jobPostingService.getJobPostingSToMain();
         model.addAttribute("postingList", postingList);
         model.addAttribute("page","main/main-content");
+
+        log.info("이것은 이름입니다.:{}",customOAuth2User.getName());
+
 
         return "main/main";
     }
