8c65420a56c96700319d5e60182439c24edb3b58
DarWook <m4768@naver.com>
2024-08-14 09:46:02 +0900

    글 보기 페이지 제외

--- src/main/java/com/jjobkorea/configuration/SecurityConfig.java
+++ src/main/java/com/jjobkorea/configuration/SecurityConfig.java
@@ -26,6 +26,8 @@ public class SecurityConfig {
                 .requestMatchers("/", "/login", "/register").permitAll() //메인, 로그인, 회원가입
                 .requestMatchers("/jobPosts", "/searchFilter").permitAll() //채용 정보
                 .requestMatchers(
+//                        "/jobseekerContent_view",
+//                                "/requestPage/jobseekerContent_view",
                                 "/getComments",
                                 "/board",
                                 "/display",
