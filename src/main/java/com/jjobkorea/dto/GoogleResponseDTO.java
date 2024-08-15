18c6292d5217509a902eb9451205ff18a6e69e3a
yyujjin <aba1740@naver.com>
2024-08-16 00:47:36 +0900

    :sparkles: GoogleResponseDTO 생성

---
+++ src/main/java/com/jjobkorea/dto/GoogleResponseDTO.java
@@ -0,0 +1,32 @@
+package com.jjobkorea.dto;
+
+import java.util.Map;
+
+public class GoogleResponseDTO {
+
+    private final Map<String,Object> attribute;
+
+    public GoogleResponseDTO(Map<String, Object> attribute) {
+        this.attribute = attribute;
+    }
+
+
+    public String getProvider() {
+        return "google";
+    }
+
+
+    public String getProviderId() {
+        return attribute.get("sub").toString();
+    }
+
+
+    public String getEmail() {
+        return attribute.get("email").toString();
+    }
+
+
+    public String getName() {
+        return attribute.get("name").toString();
+    }
+}
