29891c94ba3f0b8aca87cb0a98688d8ea8619eb5
DohoonHan <hsm3809@naver.com>
2024-08-19 17:52:53 +0900

    서버 폴더 생성 완료

--- src/main/java/com/jjobkorea/controller/ResumeController.java
+++ src/main/java/com/jjobkorea/controller/ResumeController.java
@@ -2,10 +2,6 @@ package com.jjobkorea.controller;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.Proxy;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.ParseException;
@@ -21,9 +17,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.jjobkorea.dto.ResumeInfoDTO;
 import com.jjobkorea.service.ResumeInfoService;
 import com.jjobkorea.service.UserSessionService;
@@ -37,8 +30,7 @@ import lombok.extern.slf4j.Slf4j;
 public class ResumeController {
     
     private final ResumeInfoService resumeInfoService;
-    private final AmazonS3 amazonS3;
-    private final String bucketName= "teamserverfolder";
+    
     private final UserSessionService userSessionService; 
     // 이력서 메인
     @GetMapping("/resume")
@@ -77,8 +69,9 @@ public class ResumeController {
 
     // 이력서 저장 로직
     @PostMapping("/resume/create")
-    public String addResume(@RequestParam("resumeProfilePhoto") MultipartFile file, @ModelAttribute ResumeInfoDTO resumeInfoDTO, Model model) throws ParseException, IOException {
+    public String addResume(@RequestParam("resumeProfilePhoto") MultipartFile file, @ModelAttribute ResumeInfoDTO resumeInfoDTO, Model model) throws ParseException {
         log.info("@#saveResume");
+
         String userId = userSessionService.getUserId(); //아이디 가져오기
         userSessionService.getUserName(); //이름 가져오기
         
@@ -92,31 +85,21 @@ public class ResumeController {
         }
 
         if (file != null && !file.isEmpty()) {
-            String fileName = uploadFileToS3Bucket(file);
-            resumeInfoDTO.setResumeFilePath(fileName);
+            try {
+                String fileName = uuid+"_"+file.getOriginalFilename();
+                String filePath = "D:\\dev\\upload\\" + fileName; // 실제 저장 경로로 변경
+                File dest = new File(filePath);
+                file.transferTo(dest);
+                resumeInfoDTO.setResumeFilePath(filePath);
+            } catch (IOException e) {
+                log.error("파일을 저장하지 못했습니다.", e);
+            }
         }
         resumeInfoService.insert(resumeInfoDTO);
         return "redirect:/resume";
     }
-    private String uploadFileToS3Bucket(MultipartFile file) throws IOException {
-        String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
-        
-        // 메타데이터 생성 및 설정
-        ObjectMetadata objectMetadata = new ObjectMetadata();
-        objectMetadata.setContentType(file.getContentType());
-        objectMetadata.setContentLength(file.getSize()); // 파일의 크기 설정
-        
-        // InputStream으로 파일을 읽어들임
-        try (InputStream inputStream = file.getInputStream()) {
-            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, fileName, inputStream, objectMetadata);
-            amazonS3.putObject(putObjectRequest);
-        }
-
-        return amazonS3.getUrl(bucketName, fileName).toString(); // S3 URL 반환
-    }
 //     이력서 수정 페이지 접속 로직
-    @SuppressWarnings("deprecation")
-	@GetMapping("/resume_write/edit")
+    @GetMapping("/resume_write/edit")
     public String editResume(@RequestParam("id") Long id, Model model) throws IOException {
         log.info("@#resume edit");
         
@@ -127,34 +110,22 @@ public class ResumeController {
         
         String findPath = resumeInfoService.findPhotoByUserID(id);
       	 
-        
         if (findPath != null) {
-        	if(!findPath.startsWith("http://") && !findPath.startsWith("https://")) {
-        		findPath = "http://" + findPath;
-        	}
-        	try {
-        		URL url = new URL(findPath);
-        		HttpURLConnection connection = (HttpURLConnection) url.openConnection(Proxy.NO_PROXY);
-        		connection.setRequestMethod("GET");
-        	    connection.connect();
-        		try(InputStream in = connection.getInputStream()) {
-	    			byte[] imageBytes = in.readAllBytes();
-		            String base64Image = Base64.getEncoder().encodeToString(imageBytes);
-		            model.addAttribute("image", base64Image);
-		            model.addAttribute("imageName", findPath);
-        		}
-			} catch (IOException e) {
-				log.error("이미지 파일을 로드하는데 실패하였습니다.", e);
-			}
+            byte[] imageBytes = Files.readAllBytes(Paths.get(findPath));
+            String base64Image = Base64.getEncoder().encodeToString(imageBytes);
+            model.addAttribute("image", base64Image);
+            model.addAttribute("imageName", findPath);
         }
+        
         model.addAttribute("resumeInfoDTO", resumeInfoDTO);
 
         return "resume_page/resume_edit";
     }
     // 이력서 수정 완료 업데이트 로직
     @PostMapping("/resume_write/edit")
-    public String updateResume(@RequestParam("id") Long id, @RequestParam("resumeProfilePhoto") MultipartFile file, @ModelAttribute ResumeInfoDTO resumeInfoDTO) throws IOException {
+    public String updateResume(@RequestParam("resumeId") MultipartFile file, @ModelAttribute ResumeInfoDTO resumeInfoDTO) {
         log.info("resumeUpdate");
+        UUID uuid = UUID.randomUUID();
         
         String userId = userSessionService.getUserId(); //아이디 가져오기
         userSessionService.getUserName(); //이름 가져오기
@@ -165,21 +136,26 @@ public class ResumeController {
             log.error("이력서가 존재하지 않습니다.");
             return "redirect:/resume"; // 에러 페이지로 리디렉션하거나 적절한 처리
         }
-        ResumeInfoDTO existingResume = resumeInfoService.findByIdAndUserId(id, userId);
+        ResumeInfoDTO existingResume = resumeInfoService.findByIdAndUserId(resumeInfoDTO.getId(), userId);
         if (existingResume == null) {
             log.error("존재하지 않는 이력서입니다.");
             return "redirect:/resume"; // 에러 페이지로 리디렉션하거나 적절한 처리
         }
 
         if (file != null && !file.isEmpty()) {
-        	String fileName = uploadFileToS3Bucket(file);
-            resumeInfoDTO.setResumeFilePath(fileName);
-        } else {
+            try {
+            	String fileName = uuid+"_"+file.getOriginalFilename();
+                String filePath = "D:\\dev\\upload\\" + fileName; // 실제 저장 경로로 변경
+                File dest = new File(filePath);
+                file.transferTo(dest);
+                resumeInfoDTO.setResumeFilePath(filePath);
+            } catch (IOException e) {
+                log.error("파일을 저장하지 못했습니다.", e);
+            }
+        }else {
             // 새로운 파일이 업로드되지 않았으면 기존 파일 경로를 유지합니다.
-        	resumeInfoDTO.setResumeFilePath(existingResume.getResumeFilePath());
-//            resumeInfoDTO.setResumeProfilePhoto(existingResume.getResumeProfilePhoto());
+            resumeInfoDTO.setResumeFilePath(existingResume.getResumeFilePath());
         }
-        log.info("이력서를 업데이트 했습니다: {}", resumeInfoDTO);
         resumeInfoService.update(resumeInfoDTO);
         return "redirect:/resume";
     }
@@ -189,11 +165,6 @@ public class ResumeController {
     	log.info("@#delete");
     	String userId = userSessionService.getUserId(); //아이디 가져오기
         userSessionService.getUserName(); //이름 가져오기
-        
-        ResumeInfoDTO resumeInfoDTO = resumeInfoService.findByIdAndUserId(id, userId);
-        if (resumeInfoDTO != null && resumeInfoDTO.getResumeFilePath() != null) {
-            amazonS3.deleteObject(bucketName, resumeInfoDTO.getResumeFilePath());
-        }
 
     	resumeInfoService.delete(id, userId);
         
