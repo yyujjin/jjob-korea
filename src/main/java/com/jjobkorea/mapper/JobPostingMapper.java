af629f1eb7db8a7aee49bed9e4d43abbe4f920e7
yyujjin <aba1740@naver.com>
2024-08-19 19:38:40 +0900

    companyId 추가해서 공고등록하는 mapper, xml로 수정

--- src/main/java/com/jjobkorea/mapper/JobPostingMapper.java
+++ src/main/java/com/jjobkorea/mapper/JobPostingMapper.java
@@ -29,6 +29,7 @@ public interface JobPostingMapper {
 
 	// 채용공고등록
 	public void addpostingwrite(JobPostingDTO jobPostingDTO);
+
 	//채용공고등록이미지
 //	public void insertFile(AttachFileDTO attachFile);
 }
